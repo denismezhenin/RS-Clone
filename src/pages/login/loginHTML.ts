529cd53b4ed313377ecba40980e958189424d15e
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-02-02 21:01:55 +0300

    fix: merge commit

--- src/pages/login/loginHTML.ts
+++ src/pages/login/loginHTML.ts
@@ -1,4 +1,4 @@
-const loginHTML = `<div class="login">
+const loginHTML = (page: ) => `<div class="login">
   <form action="" onsubmit="return false" class="login__form">
     <fieldset>
       <legend class="login__form-tittle">Log in</legend>
